1a9c42a247ba6b16802be9ec154a9236adbf8947
CamPetrey <cam.petrey7@gmail.com>
2026-04-22 15:00:29 -0400

    Loading new api file paths into scripts.>

---
+++ src/scripts/API/config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+  finnhubKey: 'd72304pr01qjeeefmqu0d72304pr01qjeeefmqug', //hardcoded for now 
+  finnhubRoot: 'https://finnhub.io/api/v1',
+  geminiKey: 'AIzaSyCgTUgiY9S8llgr2bLEMktQHbi_EFc1cpg',
+  geminiRoot: ''
+};
--- src/scripts/config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-const config = {
-  key: 'd72304pr01qjeeefmqu0d72304pr01qjeeefmqug', //hardcoded for now 
-  root: 'https://finnhub.io/api/v1'
-};
