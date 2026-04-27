b91d66bc64bd7c39e21b5b7030f6596235024542
CamPetrey <cam.petrey7@gmail.com>
2026-04-27 15:03:34 -0400

    Switched gemini key to new key with added restrictions to stop getting banned

--- src/scripts/API/config.js
+++ src/scripts/API/config.js
@@ -2,6 +2,6 @@
 const config = {
   finnhubKey: 'd72304pr01qjeeefmqu0d72304pr01qjeeefmqug', //hardcoded for now 
   finnhubRoot: 'https://finnhub.io/api/v1',
-  geminiKey: 'AIzaSyD0uCspWln2FpgYe-aI864amk4Pep-6nCI',
+  geminiKey: 'AIzaSyCi_PVCdkVnlnmU8GPm2MtrTH67izIWxf0',
   geminiRoot: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash'
 };
