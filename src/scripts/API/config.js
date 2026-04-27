4ddeac890da83b00441056e77238826ac576042c
CamPetrey <cam.petrey7@gmail.com>
2026-04-27 16:04:24 -0400

    remove config from tracking

--- src/scripts/API/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const config = {
-  finnhubKey: 'd72304pr01qjeeefmqu0d72304pr01qjeeefmqug', //hardcoded for now 
-  finnhubRoot: 'https://finnhub.io/api/v1',
-  geminiKey: 'AIzaSyCi_PVCdkVnlnmU8GPm2MtrTH67izIWxf0',
-  geminiRoot: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash'
-};
