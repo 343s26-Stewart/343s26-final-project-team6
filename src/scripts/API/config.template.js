3a0dcbe65de0c248e4a10410aa142bfa0f1d496a
jay6o <richforeverrr6@proton.me>
2026-05-06 16:50:12 -0400

    try 3

--- src/scripts/API/config.template.js
+++ src/scripts/API/config.template.js
@@ -1,6 +1,6 @@
 const config = {
-  finnhubKey: "",
+  finnhubKey: "FINNHUB_KEY",
   finnhubRoot: "https://finnhub.io/api/v1",
-  geminiKey: "",
+  geminiKey: "GEMINI_KEY",
   geminiRoot: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash",
 };
