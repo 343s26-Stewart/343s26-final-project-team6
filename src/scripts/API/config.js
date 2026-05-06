f2a2eab62374132d9551dabc02bdcd8749dd0d99
jay6o <richforeverrr6@proton.me>
2026-05-06 16:35:54 -0400

    deploy attempt

---
+++ src/scripts/API/config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+  geminiKey: 'GEMINI_KEY_PLACEHOLDER',
+  geminiRoot: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash',
+  finnhubKey: 'FINNHUB_KEY_PLACEHOLDER',
+ finnhubRoot: 'https://finnhub.io/api/v1',
+}
