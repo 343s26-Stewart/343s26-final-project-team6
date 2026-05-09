c5a77b43301fd557b231374e0dbefe9b62a9391a
jay6o <jay6o@users.noreply.github.com>
2026-05-09 19:01:35 +0000

    deploy: 8646c0af7822876bdbc107e5e41becef53d00b93

--- scripts/API/config.js
+++ scripts/API/config.js
@@ -1,7 +1,5 @@
 // config.js
 const config = {
-  geminiKey: 'AIzaSyBP9yrGhqTF0KkLCDdmt3U498ruPAddc7w',
-  geminiRoot: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash',
-  finnhubKey: 'd72304pr01qjeeefmqu0d72304pr01qjeeefmqug',
- finnhubRoot: 'https://finnhub.io/api/v1',
+  geminiRoot: 'https://fragrant-river-a7de.richforeverrr6.workers.dev/gemini',
+ finnhubRoot: 'https://fragrant-river-a7de.richforeverrr6.workers.dev/finnhub',
 }
